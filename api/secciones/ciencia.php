d220bb13a26feb21bc4b6a648a6ebb00b8010cdc
Angie Lilibeth Rios Custodio <134736392+alriosc@users.noreply.github.com>
2025-06-09 12:13:04 -0500

    Update ciencia.php

--- api/secciones/ciencia.php
+++ api/secciones/ciencia.php
@@ -1,11 +1,11 @@
 <?php
 /*****
-SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
+Noticia de interés en la categoría ciencia - junio 2025
 *****/
 
 $ciencia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "NASA detecta posible señal de agua líquida bajo la superficie de Encélado",
+    "autor" => "Agencia Espacial Estadounidense (NASA)",
+    "resumen" => "Un equipo de científicos de la NASA ha identificado señales consistentes con la presencia de agua líquida bajo la corteza helada de Encélado, una luna de Saturno. Este descubrimiento refuerza la posibilidad de que existan condiciones para la vida microbiana en otros cuerpos del sistema solar. Las observaciones fueron realizadas gracias a datos de la misión Cassini y recientes simulaciones térmicas."
 ];
 ?>
