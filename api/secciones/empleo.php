57f150563e21ecdd15eaa93ffef1f45abdef0531
Angie Lilibeth Rios Custodio <134736392+alriosc@users.noreply.github.com>
2025-06-09 12:13:48 -0500

    Update empleo.php

--- api/secciones/empleo.php
+++ api/secciones/empleo.php
@@ -1,11 +1,12 @@
 <?php
 /*****
-SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
+Noticia de interés en la categoría empleo - junio 2025
 *****/
 
 $empleo = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Perú lanza programa nacional para impulsar el empleo juvenil en tecnología",
+    "autor" => "Ministerio de Trabajo y Promoción del Empleo del Perú",
+    "resumen" => "El Gobierno peruano anunció un nuevo programa de empleabilidad que beneficiará a más de 50,000 jóvenes entre 18 y 29 años. La iniciativa se centra en la capacitación técnica en áreas como desarrollo web, ciberseguridad e inteligencia artificial, y vinculación directa con empresas tecnológicas que buscan talento joven capacitado."
 ];
 ?>
+
