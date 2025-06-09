d1953cd9772b2b5ccc1674fec00962940576d86f
Angie Lilibeth Rios Custodio <134736392+alriosc@users.noreply.github.com>
2025-06-09 12:14:18 -0500

    Update gente.php

--- api/secciones/gente.php
+++ api/secciones/gente.php
@@ -1,11 +1,11 @@
 <?php
 /*****
-SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
+Noticia de interés en la categoría gente - junio 2025
 *****/
 
 $gente = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Joven peruana gana premio internacional por proyecto de reciclaje inteligente",
+    "autor" => "Redacción El Comercio",
+    "resumen" => "María Fernanda López, estudiante de ingeniería ambiental en Lima, recibió el 'Premio Joven Innovador 2025' otorgado por la ONU por desarrollar un sistema de reciclaje inteligente que clasifica residuos con visión artificial. Su proyecto busca ser implementado en municipios del país para mejorar la gestión de residuos sólidos y generar empleo local."
 ];
 ?>
